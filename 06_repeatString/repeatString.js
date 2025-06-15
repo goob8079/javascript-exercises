cb9f351041b7c98acdf0299a7a75df786f75763f
Justice <floopfloop456@gmail.com>
2025-06-15 22:16:34 +0900

    1st commit

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let holder = '';
 
+    if (num < 0) return 'ERROR';
+    
+    
+    for (let i = 0; i < num; i++) {
+        holder += string;
+    }
+
+    return holder;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value1, value2 = null, value3 = null, value4 = null) {
+
+    let newArr = [] 
+    const toRemove = [value1, value2, value3, value4];
+    newArr = arr.filter(item => !toRemove.includes(item));
+
+    return newArr;
 };
 
 // Do not edit below this line
