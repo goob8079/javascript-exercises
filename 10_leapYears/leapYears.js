5555f77a1815d74569688ec099d31e54b977a948
Justice <floopfloop456@gmail.com>
2025-06-16 13:27:51 +0900

    2nc commit

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true; // if divisible by 4, 100, and 400, is leap year
+            }
+            return false; // if divisible by 4 and 100, not leap year
+        }
+        return true; // if divisble by 4 and not 100, is leap year
+    }
+    return false; // if not divisible by 4, not leap year
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(num) {
+  const fromF = (num - 32) * (5/9);
+  return Math.round(fromF * 10) / 10;
+};  
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const fromC = (num * (9/5) + 32);
+  return Math.round(fromC * 10) / 10;
 };
 
 // Do not edit below this line
